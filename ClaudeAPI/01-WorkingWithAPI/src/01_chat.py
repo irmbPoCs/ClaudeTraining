393dd7e269c217dd53ba13b9e726685ad12cd663
Raul Morales <irmoralesb@hotmail.com>
2026-07-12 22:56:03 -0600

    Refactor chat functionality to improve message handling and system message integration

--- ClaudeAPI/01-WorkingWithAPI/src/01_chat.py
+++ ClaudeAPI/01-WorkingWithAPI/src/01_chat.py
@@ -1,20 +1,30 @@
 from dotenv import load_dotenv
 from chat_helpers import *
-from anthropic.types import MessageParam
 import os
 
 load_dotenv()
 
-user_message : str = ""
-chat_history :list[MessageParam] = []
+user_message: str = ""
 
 print("Start chatting:")
 
-chat_manager = ChatHelper(os.environ.get("ANTHROPIC_API_KEY"))
+chat_setup = """
+You are an assistan for research and recommend the best practices about software development
+
+Your recommendations must consider:
+* SOLID Principles
+* Design Patterns
+* Focus on Enterprise Software
+
+IMPORTANT:
+* Do not provide information about different topics than software development
+"""
+
+chat_manager = ChatHelper(os.environ.get(
+    "ANTHROPIC_API_KEY"), system_message=chat_setup)
 
 while user_message != 'quit!':
-    user_message = input("(User)> ")
-    chat_manager.add_user_message(message=user_message, chat_history= chat_history)
-    response = chat_manager.send_conversation(chat_history=chat_history)
-    chat_manager.add_assistant_message(message=response, chat_history=chat_history)
-    chat_manager.print_last_message(chat_history=chat_history)
+    user_message = input(f"{Colors.USER}(User)> {Colors.RESET}")
+    chat_manager.add_user_message(message=user_message)
+    chat_manager.send_conversation()
+    # chat_manager.add_assistant_message(message=response)    #chat_manager.print_last_message()
--- ClaudeAPI/01-WorkingWithAPI/src/chat_helpers.py
+++ ClaudeAPI/01-WorkingWithAPI/src/chat_helpers.py
@@ -8,37 +8,50 @@ client = Anthropic()
 
 USER_ROLE = "user"
 ASSISTANT_ROLE = "assistant"
+SYSTEM_ROLE = "system"
+
+
+class Colors:
+    USER = "\033[94m"  # Blue
+    ASSISTANT = "\033[92m"  # Green
+    RESET = "\033[0m"  # Reset to default
 
 
 class ChatHelper:
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, system_message: str = ""):
         self.client = Anthropic(api_key=api_key)
+        self.system_message = None if system_message == "" else system_message
+        self.chat_history: list[MessageParam] = []
+        
 
-    def add_user_message(self, message: str, chat_history: list[MessageParam]) -> None:
-        self.add_message(message=message, role=USER_ROLE,
-                         chat_history=chat_history)
+    def add_user_message(self, message: str) -> None:
+        self.add_message(message=message, role=USER_ROLE)
 
-    def add_assistant_message(self, message: str, chat_history: list[MessageParam]) -> None:
-        self.add_message(message=message, role=ASSISTANT_ROLE,
-                         chat_history=chat_history)
+    def add_assistant_message(self, message: str) -> None:
+        self.add_message(message=message, role=ASSISTANT_ROLE)
 
-    def add_message(self, message: str, role: str, chat_history: list[MessageParam]) -> None:
-        chat_history.append(
+    def add_message(self, message: str, role: str) -> None:
+        self.chat_history.append(
             {
                 "role": role,
                 "content": message
             }
         )
 
-    def print_last_message(self, chat_history: list[MessageParam]):
-        last_message = chat_history[-1]
-        print(f"(Assistant)> {last_message["content"]}")
+    def print_message(self, user_role, message, color):
+        print(f"{color}({user_role})> {message}{Colors.RESET}")
 
-    def send_conversation(self, chat_history: list[MessageParam]):
+    def send_conversation(self):
+
         response = self.client.messages.create(
             model=model,
             max_tokens=MAX_TOKENS,
-            messages=chat_history
+            messages=self.chat_history,
+            system=self.system_message
         )
-        return response.content[0].text
+
+        assistant_message = response.content[0].text
+        self.add_assistant_message(assistant_message)
+        self.print_message(ASSISTANT_ROLE, assistant_message, Colors.ASSISTANT)
+        return assistant_message
